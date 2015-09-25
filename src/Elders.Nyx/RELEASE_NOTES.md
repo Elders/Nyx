47346d14f457e6fabe028e488ca0ce0e3699a5d2
mynkow <mynkow@gmail.com>
2015-09-25 10:56:15 +0300

    Properly invoke command to install dependencies in Init.ps1

--- src/Elders.Nyx/RELEASE_NOTES.md
+++ src/Elders.Nyx/RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.7.4 - 25.09.2015
+* Properly invoke command to install dependencies in Init.ps1
+
 #### 0.7.3 - 25.09.2015
 * Add param($installPath, $toolsPath, $package, $project) in Init.ps1
 
--- src/Elders.Nyx/deployment/Init.ps1
+++ src/Elders.Nyx/deployment/Init.ps1
@@ -1,9 +1,10 @@
-param($installPath, $toolsPath, $package, $project)
+param($installPath, $toolsPath, $package)
 
-$nuget = $Env:LocalAppData\NuGet.exe
+$nuget = $Env:LocalAppData + "\NuGet\NuGet.exe"
+Write-Host "Using NuGet from: " + $nuget
 
 Write-Host "Downloading latest version of FAKE..."
-$nuget install FAKE -OutputDirectory $toolsPath -Version 4.4.4
+& $nuget install FAKE -OutputDirectory D:\ -Version 4.4.4
 	
 Write-Host "Downloading latest version of Nuget.Core..."
-$nuget install Nuget.Core -OutputDirectory $toolsPath -Version 2.8.6
+& $nuget install Nuget.Core -OutputDirectory D:\ -Version 2.8.6
