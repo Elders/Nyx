4fb201958b524da7220f4dcde5d1f97fb4a105b8
mynkow <mynkow@gmail.com>
2017-05-31 15:06:51 +0300

    Adds script for website setup

--- .nyx/nyx.cmd
+++ .nyx/nyx.cmd
@@ -4,3 +4,5 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-pandora.cli.cmd','.nyx\get-pandora.cli.cmd')"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-deploy-dependencies.cmd','.nyx\get-deploy-dependencies.cmd')"
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-mspec.cmd','.nyx\get-mspec.cmd')"
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/setup-website.ps1','.nyx\setup-website.ps1')"
