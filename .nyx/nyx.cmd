355d50ac0288d000e1b4d102b003a58a1f51c8fc
mynkow <mynkow@gmail.com>
2017-07-27 08:49:08 +0300

    Move files around

--- .nyx/nyx.cmd
+++ .nyx/nyx.cmd
@@ -1,12 +1,4 @@
 @echo off
 
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-dependencies.cmd','.nyx\get-dependencies.cmd')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-pandora.cli.cmd','.nyx\get-pandora.cli.cmd')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-deploy-dependencies.cmd','.nyx\get-deploy-dependencies.cmd')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-mspec.cmd','.nyx\get-mspec.cmd')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-nyx.cmd','.nyx\get-nyx.cmd')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-fake.cmd','.nyx\get-fake.cmd')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/get-gitversion.cmd','.nyx\get-gitversion.cmd')"
-
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/setup-website.ps1','.nyx\setup-website.ps1')"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/setup-website-webapp.ps1','.nyx\setup-website-webapp.ps1')"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "New-Item -ItemType directory -Path .nyx\ -Force; (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Elders/Nyx/master/.nyx/.nyx.zip','.nyx\.nyx.zip')"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Expand-Archive '.nyx\.nyx.zip' -DestinationPath '.nyx\'"
